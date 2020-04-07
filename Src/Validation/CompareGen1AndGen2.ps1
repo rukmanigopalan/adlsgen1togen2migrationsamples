ad16e78636f8449a49b921f38c2a78d60bfab67e
v-wiya <62351942+v-wiya@users.noreply.github.com>
2020-04-06 18:15:38 -0700

    Add files via upload

---
+++ Src/Validation/CompareGen1AndGen2.ps1
@@ -0,0 +1,84 @@
+﻿##################################################################################################################################
+# A script to compare all gen2 files for existance and file size.
+# 
+# Prerequisite:
+#  1. need to have the azure subscription
+#  2. ReourceGroup
+#  3. Azure Storage account and Azure storage container and Azure datalake gen1 account
+#  4. Service principal that has permission on the subscription
+#  5. All Azure login information such as tenantId, service principal need to store in Azure keyvault. 
+# Steps the operation does:
+#  1. executing getgen1files and gen2files scripts
+#  2. set gen1 and gen2 files in hashtable
+#  3. Check if gen2 files are exist in gen1
+#  4. if exist, then check if gen2 files sizes are same
+#    Sample Usage:
+#
+#$diff = C:\Users\v-wiya\Documents\CompareGen1AndGen2.ps1 -Gen1Files $gen1FileDetails -Gen2Files $gen2FileDetails -outputPath "C:\Users\v-wiya\Documents\diff.csv"
+#
+# 
+
+param(
+[System.Collections.ICollection] $Gen1Files,
+[System.Collections.ICollection] $Gen2Files,
+[string] $outputPath
+)
+    try{
+    #set up a datatable to store difference
+        $filediff=New-Object System.Data.DataTable;
+        $filediff.Columns.Add((New-Object System.Data.DataColumn 'Gen1Files', ([string])));
+        $filediff.Columns.Add((New-Object System.Data.DataColumn 'Gen2Files', ([string])));
+        $filediff.Columns.Add((New-Object System.Data.DataColumn 'difference', ([string])));
+
+        #set up hastable for gen1 files
+        $gen1Dict = New-Object 'system.collections.generic.dictionary[string,int]'
+        foreach ($eachRow in $gen1FileDetails)
+        {
+            if ($eachRow.Path -ne $null)
+            {
+            $gen1Dict.Add($eachRow.Path, $eachRow.length);
+            }
+        }
+
+          #set up hastable for gen2 files
+        $gen2Dict = New-Object 'system.collections.generic.dictionary[string,int]'
+        foreach ($eachRow in $gen2FileDetails)
+        {
+            if ($eachRow.Path -ne $null)
+            {
+            $gen2Dict.Add($eachRow.Path, $eachRow.length);
+            }
+        }
+
+        foreach ($eachRow in $gen1Dict)
+        {
+            if ($eachRow.key -ne $null)
+            {
+                #check if gen2 files exist in gen1
+                if ($gen2Dict.ContainsKey($eachRow.key))
+                {
+                    if ($gen1Dict[$eachRow.key] -ne $gen2Dict[$eachRow.key] )
+                    {
+                    #check if gen2 files have same size as in gen2
+                        $filediff.Rows.Add($eachRow.key,$eachRow.key, 'length not same');
+                    }
+
+                }
+                else
+                {
+                    $filediff.Rows.Add($eachRow.key,' ', 'Gen2 Missing files');
+                }
+            }
+        }
+    }
+    catch{
+        throw $error[0].Exception;   
+    }
+    
+    return $filediff;
+
+
+function ExportCSV{
+    param([System.Collections.ICollection]$files, [string] $path )
+    $files|Export-Csv -Path $path
+}
---
+++ Src/Validation/GetGen1Inventory.ps1
@@ -0,0 +1,74 @@
+﻿##################################################################################################################################
+# A script to get all gen1 files to array
+# 
+# Prerequisite:
+#  1. need to have the azure subscription
+#  2. Azure Datalake storage gen1 account
+#  3. Service principal that has permission on the subscription
+#  4. All Azure login information such as tenantId, service principal need to store in Azure keyvault. 
+# Steps the operation does:
+#  1. Connect to Azure using password and tenantid 
+#  2. Get files in gen1 
+#  3. The Output will be array of object in memory 
+#  4. Starting from the Gen1FilePath, get all the folders and subfolders recursively and stored them in the array.
+# Sample Usage:
+#
+#$gen1FileDetails = C:\Users\v-wiya\Documents\GetGen1Inventory.ps1 -subscriptionId  -keyVaultName "Gen2MigrationKV" -tenantIdKVSecreatName "72f988bf-86f1-41af-91ab-2d7cd011db47" -keyVaultServicePrincipalSecretName "Zjl/J.vWJyQ50u]@HOOeVkQwRPKiNw31" -keyVaultServicePrincipalIdSecretName "55b7545f-661a-4710-b336-ea1e3c474d09" -FilePath $filepath -AccountName "sourcedatalakestoregen1" -cutofftime "1/1/1970"
+#
+#
+# 
+param(
+[string]$subscriptionId,
+[string]$keyVaultName,
+[string] $tenantIdKVSecreatName,
+[string]$keyVaultServicePrincipalSecretName,
+[string]$keyVaultServicePrincipalIdSecretName,
+[string] $FilePath,
+[string] $AccountName,
+[string] $cutofftime
+)
+
+function GetGen1DataList {
+    [cmdletBinding()]
+    param([string] $FilePath, [string] $AccountName, [string] $cutofftime)
+
+    #convert cutofftime to int
+    if ($cutofftime -eq '')
+    {
+    $cutoffint=0;
+    }
+    else{
+    $cutoffint=[int64]((get-date $cutofftime)-(get-date "1/1/1970")).TotalMilliseconds;
+    }
+    #set up array to hold DataLakeStore items
+    $Files=New-Object Collections.Generic.List[Microsoft.Azure.Commands.DataLakeStore.Models.DataLakeStoreItem];
+    if ($FilePath -ne '' -and $AccountName -ne ''){
+        $ChildItems = Get-AzDataLakeStoreChildItem -AccountName $AccountName -Path $FilePath;
+        foreach ($ChildItem in $ChildItems) {
+            switch ($ChildItem.Type) {
+                "FILE" {
+                    if ($ChildItem.modificationtime -gt $cutoffint){
+                        $Files+=$ChildItem; 
+                        }
+                }
+                "DIRECTORY" {
+                    #if the file is directory, then go find the files of the directory
+                    $Files+=GetGen1DataList $ChildItem.path $AccountName $cutofftime;
+                }
+            }
+        }       
+    }
+    else{
+        throw 'cannot find filepath or account';
+    }  
+    return $Files;
+}
+
+   # connect to azure using service principal and select the subscription to use
+ C:\Users\v-wiya\Documents\ConnectToAzure.ps1 -subscriptionId $subscriptionId -keyVaultName $keyVaultName -tenantIdKVSecreatName $tenantIdKVSecreatName -servicePrincipalSecreatKVSecreatName $keyVaultServicePrincipalIdSecretName -servicePrincipalIDKVSecreatName $keyVaultServicePrincipalIdSecretName
+  
+  #call function
+  GetGen1DataList $FilePath $AccountName $cutofftime;
+
+
+
