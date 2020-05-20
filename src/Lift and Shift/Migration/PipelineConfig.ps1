f5241030cabc927c4c30da04590c896d3ff624c9
v-zumeha <62354039+v-zumeha@users.noreply.github.com>
2020-05-20 06:30:07 +0000

    Adding Lift and Shift (#42)

---
+++ src/Lift and Shift/Migration/PipelineConfig.ps1
@@ -0,0 +1,123 @@
+﻿Param(  
+    [string]$inputConfigFilePath 
+)
+
+$fileRootPath = $PSScriptRoot + "\"
+$sourceInputFilePath = $inputConfigFilePath
+$outputDesFileFullName = "SourceConfig.json"
+$sourceInputData = Get-Content -Raw -Path $sourceInputFilePath | ConvertFrom-Json
+$gen1SourceRootPath = $sourceInputData.gen1SourceRootPath
+$tenantId = $sourceInputData.tenantId
+$subscriptionId = $sourceInputData.subscriptionId
+$gen2SourceRootPath = $sourceInputData.gen2DestinationRootPath
+$overwrite = $sourceInputData.overwrite
+$resourceGroupName = $sourceInputData.resourceGroupName
+$factoryName = $sourceInputData.factoryName
+$location = $sourceInputData.location
+
+$adlsGen1LSName = "AzureDataLakeStoreGen1"
+$adlsGen1LSType = "AzureDataLakeStore"
+$adlsGen1DSLocationType = "AzureDataLakeStoreLocation"
+$adlsGen2LSName = "AzureDataLakeStoreGen2"
+$adlsGen2LSType = "AzureBlobFS"
+$adlsGen2DSLocationType = "AzureBlobFSLocation"
+$pipelineCopyType = "Binary"
+
+$jsonBase = @{ }
+$factoriesList = New-Object System.Collections.ArrayList
+$ls = New-Object System.Collections.ArrayList
+$ds = New-Object System.Collections.ArrayList
+$pipelines = New-Object System.Collections.ArrayList
+
+try {
+
+#Create Linked services definitions for both Gen1 & Gen2
+
+    $gen1LSProperties = @{"type" = $adlsGen1LSType ; "dataLakeStoreUri" = $gen1SourceRootPath; "servicePrincipalId" = ""; "tenant" = $tenantId; "subscriptionId" = $subscriptionId; "servicePrincipalKey" = "" }
+    $ls.Add(@{"lsId" = "1"; "name" = $adlsGen1LSName ; "overwrite" = $overwrite; "properties" = $gen1LSProperties })
+    $gen2LSProperties = @{"type" = $adlsGen2LSType ; "url" = $gen2SourceRootPath; "accountKey" = "" }
+    $ls.Add(@{"lsId" = "2"; "name" = $adlsGen2LSName ; "overwrite" = $overwrite; "properties" = $gen2LSProperties })
+    
+    $dsCount = 0
+    $pipelineCount = 0
+
+#Populate Datasets and Pipeline activities definitions    
+
+    foreach ($pipeline in $sourceInputData.pipeline) {
+        $pipelineActivityCount = 0
+        $pipelineCount++
+        
+ #For all Incremental folders, create respective Datasets and Pipeline activity definitions  
+ 
+        if ($pipeline.isChurningOrIsIncremental -eq "true") {
+            $pipelineIncActivities = New-Object System.Collections.ArrayList
+    
+            foreach ($pipelineDetails in $pipeline.pipelineDetails) {
+                $dsCount++
+                $inputAdlsGen1Name = "InputGen1ADLSInc" + $dsCount
+                
+ #Create Input & Output dataset definitions
+                
+                $ds1TypeProperties = @{ "locationType" = $adlsGen1DSLocationType; "folderPath" = $pipelineDetails.sourcePath }
+                $ds1Properties = @{ "type" = $pipelineCopyType; "typeProperties" = $ds1TypeProperties }
+                $ds.Add(@{ "dsId" = $dsCount; "name" = $inputAdlsGen1Name; "referenceName" = $adlsGen1LSName; "overwrite" = $overwrite ; "properties" = $ds1Properties })
+                
+                $inputAdlsGen2Name = "OutputGen2ADLSInc" + $dsCount
+                
+                $ds2TypeProperties = @{ "locationType" = $adlsGen2DSLocationType; "folderPath" = $pipelineDetails.destinationPath ; "fileSystem" = $pipelineDetails.destinationContainer }
+                $ds2Properties = @{ "type" = $pipelineCopyType ; "typeProperties" = $ds2TypeProperties }
+                $ds.Add(@{"dsId" = $dsCount; "name" = $inputAdlsGen2Name ; "referenceName" = $adlsGen2LSName; "overwrite" = $overwrite ; "properties" = $ds2Properties })
+               
+  #Create pipeline activity definitions          
+               
+                $pipelineActivityCount++
+                $desFullPath = $pipelineDetails.destinationContainer + "/" + $pipelineDetails.destinationPath + "/"
+                #$sourceFullPath = ($pipelineDetails.sourcePath).TrimStart("//") + "/"
+                $pipelineIncActivitiesProperties = @{ "name" = "CopyADLSGen1ToGen2Activity" + $pipelineActivityCount; "inputDataSetReferenceName" = $inputAdlsGen1Name; "outputDataSetReferenceName" = $inputAdlsGen2Name; "inputFolderPath" = $pipelineDetails.sourcePath + "/" ; "outputFolderPath" = $desFullPath }    
+                $pipelineIncActivities.Add($pipelineIncActivitiesProperties)
+    
+            }
+           
+  #Create Incremental pipeline definition
+  
+            $pipelines.Add(@{"pipelineId" = $pipeline.pipelineId; "name" = "CopyGen1ToGen2Inc" + $pipelineCount; "type" = "Copy"; "overwrite" = $overwrite ; "incremental" = "true"; "triggerName" = "CopyGen1ToGen2IncTrigger" + $pipelineCount; "triggerFrequency" = $pipeline.triggerFrequency; "triggerInterval" = $pipeline.triggerInterval; "triggerUTCStartTime" = $pipeline.triggerUTCStartTime; "triggerUTCEndTime" = $pipeline.triggerUTCEndTime; "activities" = $pipelineIncActivities })
+        }
+   #For all Full load folders, create respective Datasets and Pipeline activity definitions 
+        if ($pipeline.isChurningOrIsIncremental -eq "false") {
+            $pipelineActivities = New-Object System.Collections.ArrayList
+            foreach ($pipelineDetails in $pipeline.pipelineDetails) {
+                $dsCount++
+                $inputAdlsGen1Name = "InputGen1ADLSFull" + $dsCount
+  #Create Input & Output dataset definitions                                                  
+                $ds1TypeProperties = @{ "locationType" = $adlsGen1DSLocationType; "folderPath" = $pipelineDetails.sourcePath }
+                $ds1Properties = @{ "type" = $pipelineCopyType; "typeProperties" = $ds1TypeProperties }
+                $ds.Add(@{ "dsId" = $dsCount; "name" = $inputAdlsGen1Name; "referenceName" = $adlsGen1LSName; "overwrite" = $overwrite ; "properties" = $ds1Properties })
+                
+                $dsCount++
+                $inputAdlsGen2Name = "OutputGen2ADLSFull" + $dsCount
+                
+                $ds2TypeProperties = @{ "locationType" = $adlsGen2DSLocationType; "folderPath" = $pipelineDetails.destinationPath ; "fileSystem" = $pipelineDetails.destinationContainer }
+                $ds2Properties = @{ "type" = $pipelineCopyType ; "typeProperties" = $ds2TypeProperties }
+                $ds.Add(@{"dsId" = $dsCount; "name" = $inputAdlsGen2Name ; "referenceName" = $adlsGen2LSName; "overwrite" = $overwrite ; "properties" = $ds2Properties })
+   #Create pipeline activity definitions
+                $pipelineActivityCount++
+                $pipelineFullActivitiesProperties = @{ "name" = "CopyADLSGen1ToGen2Activity" + $pipelineActivityCount; "inputDataSetReferenceName" = $inputAdlsGen1Name; "outputDataSetReferenceName" = $inputAdlsGen2Name }    
+                $pipelineActivities.Add($pipelineFullActivitiesProperties)
+            }
+    #Create Full pipeline definition
+           $pipelines.Add(@{"pipelineId" = $pipeline.pipelineId; "name" = "CopyGen1ToGen2Full" + $pipelineCount; "type" = "Copy"; "overwrite" = $overwrite ; "incremental" = "false"; "activities" = $pipelineActivities })
+        }
+        
+    }
+    #Populate details for Data factory definition
+    
+    $factoriesList.Add(@{"factoryName" = $factoryName; "resourceGroupName" = $resourceGroupName; "location" = $location; "linkedServices" = $ls ; "dataSets" = $ds ; "pipelines" = $pipelines })
+    $jsonBase.Add("factories", $factoriesList)
+    $output = $jsonBase | ConvertTo-Json -Depth 10
+    $configFilePath = $fileRootPath + $outputDesFileFullName
+    Set-Content -Path $configFilePath -Value $output
+
+}
+catch {
+    throw $error[0].Exception
+} 
---
+++ src/Lift and Shift/Validation/InvokeValidation.ps1
@@ -0,0 +1,59 @@
+﻿
+# This script gets the Gen1, Gen2 file details, compares it and export result to CSV 
+
+Param(  
+    [string]$inputConfigFilePath,
+    [hashtable] $pipelineIds
+)
+
+Import-module az.storage
+Import-Module az.datalakestore
+
+$sourceConfigFullPath = $inputConfigFilePath
+$sourceConfigData = Get-Content -Raw -Path $sourceConfigFullPath | ConvertFrom-Json
+
+$subscriptionId = $sourceConfigData.subscriptionId
+$resourceGroupName = $sourceConfigData.resourceGroupName
+$gen1SourceRootPath = $sourceConfigData.gen1SourceRootPath
+$gen2SourceRootPath = $SourceConfigData.gen2DestinationRootPath
+$gen1AccountName = $gen1SourceRootPath.Substring(8, $gen1SourceRootPath.indexOf(".") - 8)
+$gen2AccountName = $gen2SourceRootPath.Substring(8, $gen2SourceRootPath.indexOf(".") - 8)
+$currentDate = (Get-Date).ToString("yyyyMMddHHmm")
+$validationResultFolder = $PSScriptRoot + "\Output\" + $currentDate + "\"
+$LogPath = $PSScriptRoot + "\Log\" + "ExecutionLog_" + $CurrentDate + ".txt" 
+
+Start-Transcript -Path $LogPath -Append
+
+If (!(Test-path $validationResultFolder)) {
+    New-Item -ItemType Directory -Force -Path $validationResultFolder
+    Write-Host "`nCreated below directory to store validation result files `n" -ForegroundColor Green
+    Write-Host "$($validationResultFolder)"
+}
+
+# Get the Gen1 and Gen2 paths configured in Config file and compare details, export result to CSV
+
+foreach ($eachPipeline in $sourceConfigData.pipeline) {    
+    if ($pipelineIds.ContainsKey($eachPipeline.pipelineId)) {
+        foreach ($eachPath in $eachPipeline.pipelineDetails) {
+            $gen1Path = $eachPath.sourcePath
+            $gen2Path = $eachPath.destinationPath
+            $gen2Container = $eachPath.destinationContainer
+            $resultFilePath = $validationResultFolder + $gen1Path.Replace("/", "-") + ".csv"
+	    		    	
+            Write-Host "`n"
+            Write-Host "Getting ADL Gen1 File details" -ForegroundColor Yellow
+            Write-Host "ADL Gen1 Account: $($gen1AccountName)" 
+            Write-Host "ADL Gen1 Root Path: $($gen1Path)" 
+            Write-Host "`n"
+          
+            $gen1FileDetails = @(& "$PSScriptRoot\GetGen1Inventory.ps1" -subscriptionId $subscriptionId -filePath $gen1Path -accountName $gen1AccountName -ErrorAction Stop) 
+                     
+            $gen2FileDetails = @(& "$PSScriptRoot\GetGen2Inventory.ps1" -subscriptionId $subscriptionId -storageAccountName $gen2AccountName -resourceGroupName $resourceGroupName -gen2FilesystemName $gen2Container -gen2FilePath $gen2Path)
+            
+            & "$PSScriptRoot\CompareGen1AndGen2.ps1" -gen1Files $gen1FileDetails -gen2Files $gen2FileDetails -ValidationResultFilePath $resultFilePath `
+            
+        }
+    }
+}
+
+Stop-Transcript
